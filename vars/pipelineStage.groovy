ee52f8dc1876ad2ce6e349fbf67b735c43c49004
RitikaM09 <90257344+RitikaM09@users.noreply.github.com>
2021-09-15 15:09:12 +0530

    Update pipelineStage.groovy

--- vars/pipelineStage.groovy
+++ vars/pipelineStage.groovy
@@ -1,1 +1,19 @@
+@Field final PROPERTY_DIR="vard/my.properties"
+def initStage(parent,params){
+    for (param in params) {
+        String[] paramArray = param.split(":", 2)
+        String key = paramArray[0]
+        //if (paramArray.length == 2) {
+            String value = paramArray[1]
+            //println value
+            parent.parameterMap.put(key.toString(), value.toString())
+            println parent.parameterMap
+        //} else if (paramArray.length == 1) {
+           // parent.parameterMap.put(key.toString().trim(), "");
+        
+    }
+}
+
+
+
 
