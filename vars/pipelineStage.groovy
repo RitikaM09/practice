92e8d96fa2c485caaf988d9a4d6a7624b4bf3584
RitikaM09 <90257344+RitikaM09@users.noreply.github.com>
2021-09-30 13:57:52 +0530

    Update pipelineStage.groovy

--- vars/pipelineStage.groovy
+++ vars/pipelineStage.groovy
@@ -4,19 +4,19 @@ import groovy.transform.Field
 @Field final PROPERTY_DIR="vars/my.properties"
 
 def initStage(parent,params){
-    for (param in params) {
-        String[] paramArray = param.split(":", 2)
-        String key = paramArray[0]
-        //if (paramArray.length == 2) {
-            String value = paramArray[1]
-            //println value
-            parent.parameterMap.put(key.toString(), value.toString())
-            println parent.parameterMap
-        //} else if (paramArray.length == 1) {
-           // parent.parameterMap.put(key.toString().trim(), "");
-        println 'Ends here'
+//     for (param in params) {
+//         String[] paramArray = param.split(":", 2)
+//         String key = paramArray[0]
+//         //if (paramArray.length == 2) {
+//             String value = paramArray[1]
+//             //println value
+//             parent.parameterMap.put(key.toString(), value.toString())
+//             println parent.parameterMap
+//         //} else if (paramArray.length == 1) {
+//            // parent.parameterMap.put(key.toString().trim(), "");
+//         println 'Ends here'
         
-    }
+//     }
 }
 
 def readPropertyFile(p){
